fa579cb1f5db2a173cca156e5ff716146ba3f223
PaolaTrevino3 <ptrevinochapa@byupathway.edu>
2026-05-12 01:22:04 -0500

    w1

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+JavaScript
+
+const currentYear = document.querySelector(".current-year");
+const lastModified = document.querySelector(".last-modified");
+
+currentYear.textContent = new Date().getFullYear();
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
