059e112b2bd5d26bef823befd8e7d18a207ab90b
PaolaTrevino3 <ptrevinochapa@byupathway.edu>
2026-05-23 16:27:10 -0500

    w03 hw

---
+++ scripts/place.js
@@ -0,0 +1,18 @@
+const temperature = 10;
+const windSpeed = 5;
+
+function calculateWindChill(temp, speed) {
+    return(13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(2);
+}
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    document.getElementById("windChill").textContent = calculateWindChill(temperature, windSpeed) + " °C";
+} else {
+    document.getElementById("windChill").textContent = "N/A";
+}
+
+const year = document.getElementById('year');
+year.textContent = new Date().getFullYear();
+
+const lastModified = document.getElementById('lastModified');
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
